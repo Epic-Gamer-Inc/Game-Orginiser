2ebda0dc508c076aee5b50600ece620aa67e88c6
Pricetag_ <jakesemail098@gmail.com>
2020-09-09 08:43:12 +0930

    no message

--- Main.py
+++ Main.py
@@ -4,6 +4,7 @@ from flask import *
 import dataset
 import random
 import string
+from sqlalchemy.sql import text
 
 app = Flask(__name__)
 db = dataset.connect('sqlite:///DataBase.db')
@@ -29,7 +30,6 @@ def login_post():
         session['name'] = request.form['username']
         session['id'] = db_user['id']
         session['profilePic'] = db_user['profilePic']
-        session['teamName'] = getTeamName(db_user['team'])
         return redirect('/')
     else:
         return "Invalid Password"
@@ -64,6 +64,11 @@ def create_account_post():
 
 @app.route('/profile')
 def profile_get():
+    db_user = db['Players'].find_one(name=session['name'])
+    session['teamName'] = getTeamName(db_user['team'])
+    db_team = db['Teams'].find_one(id=db_user['team'])
+    session['teamMembers'] = list([GetFullName(db_team['player0']),GetFullName(db_team['player1']),GetFullName(db_team['player2']),GetFullName(db_team['player3']),GetFullName(db_team['player4'])])
+    session['teamRank'] = catagorise(db_team['mmr'])
     return render_template('Profile.html', filtered_posts='')
 
 @app.route('/create_team')
@@ -87,6 +92,11 @@ def create_team_post():
     members.append(player3)
     members.append(player4)
     CreateTeam(members, request.form['teamName'])
+    db_user = db['Players'].find_one(name=session['name'])
+    session['teamName'] = getTeamName(db_user['team'])
+    db_team = db['Teams'].find_one(id=db_user['team'])
+    session['teamMembers'] = list([GetFullName(db_team['player0']),GetFullName(db_team['player1']),GetFullName(db_team['player2']),GetFullName(db_team['player3']),GetFullName(db_team['player4'])])
+    session['teamRank'] = catagorise(db_team['mmr'])
     return redirect('/')
 
 app.run(debug=True)
